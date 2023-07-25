1d3c081a2068ae3ed12dd302db25d388bab2a6ed
shahidmonowarr <shahidmonowar21@gmail.com>
2023-07-25 12:22:27 +0600

    data fetching & ISR

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import Head from "next/head";
 import RootLayout from "@/components/Layouts/RootLayout";
+import AllNews from "@/components/UI/AllNews";
 import Banner from "@/components/UI/Banner";
+import Head from "next/head";
 
-
-const HomePage = () => {
+const HomePage = ({ allNews }) => {
   return (
     <>
       <Head>
@@ -16,6 +16,7 @@ const HomePage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Banner />
+      <AllNews allNews={allNews} />
     </>
   );
 };
@@ -23,4 +24,16 @@ export default HomePage;
 
 HomePage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/news");
+  const data = await res.json();
+
+  return {
+    props: {
+      allNews: data,
+    },
+    revalidate: 10,
+  };
 };
